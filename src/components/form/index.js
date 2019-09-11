17d67e7374852ba0b6ec751a5d57599e87f3396e
CarlosZiegler <carlos.ziegler@gmail.com>
2019-09-11 17:27:36 +0200

    Form Button

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -18,22 +18,19 @@ const Formular = () => (
                 </Col>
                 <Col>
                     <Form>
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email"/>
-                            <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                            </Form.Text>
-                        </Form.Group>
                         <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password"/>
+                            <Form.Label>Ihre Name</Form.Label>
+                            <Form.Control type="text" placeholder="Enter email"/>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Ihre Email*</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email"/>
                         </Form.Group>
                         <Form.Group controlId="exampleForm.ControlTextarea1">
-                            <Form.Label>Example textarea</Form.Label>
+                            <Form.Label>Ihre Nachricht*</Form.Label>
                             <Form.Control as="textarea" rows="3"/>
                         </Form.Group>
-                        <Button variant="primary" type="submit">
+                        <Button className="button__wrapper" variant="outline-primary" type="submit">
                             Submit
                         </Button>
                     </Form>
