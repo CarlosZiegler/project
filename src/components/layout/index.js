ab2795988f923252078b925c6818242b7da84613
CarlosZiegler <carlos.ziegler@gmail.com>
2019-09-11 15:45:34 +0200

    Landingpage Footer

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "../header"
 import Menu from "../menu"
+import Footer from "../footer"
 
 
 const Layout = ({ children }) => {
@@ -33,10 +34,9 @@ const Layout = ({ children }) => {
       <div>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+            <Footer/>
         </footer>
+
       </div>
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,101 +12,12 @@ const IndexPage = () => (
     <Layout>
         <SEO title="Home"/>
 
+        <Leistungen id="leistungen" />
+        <Wordpress id="wordpress" />
+        <Portfolio id="portfolio" />
+        <Formular id="kontakt" />
 
-        <Leistungen/>
-        <Wordpress/>
-        <Portfolio/>
-        <Formular/>
-
-
-
-
-        <div>
-            <p>This contains a button which is exported by react-bootstrap</p>
-
-        </div>
         <Link to="/page-2/">Go to page 2</Link>
-
-
-        <div id="leistungen">
-            <h1>leistungen</h1>
-            <p>
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum."
-            </p>
-        </div>
-
-        <div id="wordpress">
-            <h1>wordpress</h1>
-            <p>
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum."
-            </p>
-        </div>
-
-        <div id="freelancing">
-            <h1>freelancing</h1>
-            <p>
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum."
-            </p>
-        </div>
-
-        <div id="portfolio">
-            <h1>portfolio</h1>
-            <p>
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum."
-            </p>
-        </div>
-
-        <div id="testimonials">
-            <h1>testimonials</h1>
-            <p>
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum."
-            </p>
-        </div>
-
-        <div id="kontakt">
-            <h1>kontakt</h1>
-            <p>
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum."
-            </p>
-        </div>
-
-
 
     </Layout>
 )
