7cdad868c812e4f21bfeff552e71837c943e7c5d
CarlosZiegler <carlos.ziegler@gmail.com>
2019-10-28 09:32:46 +0100

    Travis

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,57 +1,57 @@
 module.exports = {
-  siteMetadata: {
-    title: `Visyu Solution`,
-    description: `Wir bieten Ihnen Lösungen für jedes Endgerät. Egal ob Desktop, Laptop, TV oder mobil. Unsere Web- und App-Lösungen passen zu Ihren Bedürfnissen.`,
-    author: `@visyu`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: `Visyu Solution`,
+        description: `Wir bieten Ihnen Lösungen für jedes Endgerät. Egal ob Desktop, Laptop, TV oder mobil. Unsere Web- und App-Lösungen passen zu Ihren Bedürfnissen.`,
+        author: `@visyu`,
     },
-    {
-      resolve: `gatsby-source-wordpress`,
-      options: {
-        // your wordpress source
-        baseUrl: `visyu.de`,
-        protocol: `https`,
-        // is it hosted on wordpress.com, or self-hosted?
-        hostingWPCOM: false,
-        // does your site use the Advanced Custom Fields Plugin?
-        useACF: false,
-        includedRoutes: [
-          "**/categories",
-          "**/posts",
-          "**/blog",
-           "**/media",
-           "**/tags",
-           "**/taxonomies",
-           "**/users",
-          "**/portfolio"
-        ],
-      }
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/logo.png`, // This path is relative to the root of the site.
-      },
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        {
+            resolve: `gatsby-source-wordpress`,
+            options: {
+                // your wordpress source
+                baseUrl: `visyu.de`,
+                protocol: `https`,
+                // is it hosted on wordpress.com, or self-hosted?
+                hostingWPCOM: false,
+                // does your site use the Advanced Custom Fields Plugin?
+                useACF: false,
+                includedRoutes: [
+                    "**/categories",
+                    "**/posts",
+                    "**/blog",
+                    "**/media",
+                    "**/tags",
+                    "**/taxonomies",
+                    "**/users",
+                    "**/portfolio"
+                ],
+            }
+        },
 
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/logo.png`, // This path is relative to the root of the site.
+            }
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
 }
